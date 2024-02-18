1e72e9b8d4a903c338a66d3e01a57a31b7233016
Aldrin Ong <0n9aldrin@gmail.com>
2024-02-17 20:04:50 -0800

    Beautify chat

--- src/screens/chat/Chat.js
+++ src/screens/chat/Chat.js
@@ -7,6 +7,9 @@ import {
   TextInput,
   onPress,
   SafeAreaView,
+  Platform,
+  KeyboardAvoidingView,
+  Image,
 } from "react-native";
 import * as WebBrowser from "expo-web-browser";
 import { useConvexAuth } from "convex/react";
@@ -16,13 +19,22 @@ import Clerk from "@clerk/clerk-js";
 import { Divider } from "react-native-elements";
 import { useQuery, useMutation } from "convex/react";
 import { api } from "../../../convex/_generated/api";
+import Icon from "react-native-vector-icons/Feather";
+
+const dummyMessages = [
+  { origin: "Austin", message: "Hello!" },
+  { origin: "User123", message: "Hi there!" },
+  { origin: "Austin", message: "How are you doing?" },
+  { origin: "User123", message: "I'm good, thanks!" },
+];
 
 const Chat = () => {
   const [emailAddress, setEmailAddress] = React.useState("");
   const [password, setPassword] = React.useState("");
   const { isLoading, isAuthenticated } = useConvexAuth();
   const [newMessageText, setNewMessageText] = useState("");
-  const messages = useQuery(api.messaging.list);
+  // const messages = useQuery(api.messaging.list);
+  const messages = dummyMessages;
   const sendMessage = useMutation(api.messaging.send);
   const btnSendMsg = async () => {
     await sendMessage({ message: newMessageText, origin: "Austin" });
@@ -31,96 +43,255 @@ const Chat = () => {
   console.log(messages);
 
   return (
-    <View style={styles.container}>
-      <View style={styles.divider}></View>
-      <SafeAreaView style={styles.msgAreaContainer}>
-        <ScrollView style={styles.msgArea}>
-          {messages?.map((message) => (
-            <View
-              key={message._id}
-              style={[
-                styles.messageContainer,
-                message.origin === "Austin" ? styles.messageMine : null,
-              ]}
-            >
-              <Text>{message.origin}</Text>
-              <Text>{message.message}</Text>
+    <SafeAreaView style={styles.safeArea}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.container}
+        keyboardVerticalOffset={Platform.OS === "ios" ? 64 : 0}
+      >
+        <View style={styles.header}>
+          <View style={styles.topHeader}>
+            <View style={styles.confirmButton1}>
+              <Icon name="check-circle" size={24} color="#EAEAEA" />
+              <Text style={styles.confirmButtonText1}>We Met</Text>
             </View>
-          ))}
-        </ScrollView>
-      </SafeAreaView>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.input}
-          autoCapitalize="none"
-          value={newMessageText}
-          placeholder="Enter text here..."
-          onChangeText={(newMsg) => setNewMessageText(newMsg)}
-        />
-        <TouchableOpacity style={styles.btn} onPress={btnSendMsg}>
-          <Text style={styles.buttonText}>Send Message</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
+            <Image
+              source={{
+                uri: "https://s3-alpha-sig.figma.com/img/fd16/f7c0/f7413b2e46fd5b05c964dd658938cd24?Expires=1708905600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=kGuzGIOB9ol9Vbu8w-W47Gyk7nKDWUWhkjjYdCyeWep4IAptjL-JPwxafzQkabUDDGVJZcZuZu8WCsQWKliOmhNVCsQUGPLfolSzpBhFBVlr~RP~8o~uQWjqOlA0-oya-~L6Ytcyrb2ufynAHcvViB~RQOS4XpdOkXVC71LzISDtpggjkwWycXJWrQyrzyMvEtu9FghZjyOJRYitP0L3-Iu0VHbo~6tZIph8zzOVbOPOvpuj71SUtwhA2uUO9-PsKhyao9TYfJ0k1mUrg8WN40~fCeH4tNAj70m~B0H0qQEi79-FJB20B8yazYq5g3Y23OhI7vT0uQDSo1L6bv-CAw__",
+              }}
+              style={styles.profileImage}
+            />
+            <View style={styles.confirmButton}>
+              <Icon name="check-circle" size={20} color="white" />
+              <Text style={styles.confirmButtonText}>We Met</Text>
+            </View>
+          </View>
+          <Text style={styles.userName}>Marvis Ighedosa</Text>
+          <Text style={styles.date}>01/01/24 11:00 AM</Text>
+          <Text style={styles.address}>69 Ur Mums Street, CA, 10001</Text>
+        </View>
+        <View style={styles.msgAreaContainer}>
+          <ScrollView style={styles.msgArea}>
+            {messages?.map((message) => (
+              <View
+                key={message._id}
+                style={[
+                  styles.messageContainer,
+                  message.origin === "Austin"
+                    ? styles.messageMine
+                    : styles.messageTheirs,
+                ]}
+              >
+                {/* Conditionally render profile picture for messages sent by users other than "Austin" */}
+                {message.origin !== "Austin" && (
+                  <Image
+                    source={{
+                      uri: "https://s3-alpha-sig.figma.com/img/fd16/f7c0/f7413b2e46fd5b05c964dd658938cd24?Expires=1708905600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=kGuzGIOB9ol9Vbu8w-W47Gyk7nKDWUWhkjjYdCyeWep4IAptjL-JPwxafzQkabUDDGVJZcZuZu8WCsQWKliOmhNVCsQUGPLfolSzpBhFBVlr~RP~8o~uQWjqOlA0-oya-~L6Ytcyrb2ufynAHcvViB~RQOS4XpdOkXVC71LzISDtpggjkwWycXJWrQyrzyMvEtu9FghZjyOJRYitP0L3-Iu0VHbo~6tZIph8zzOVbOPOvpuj71SUtwhA2uUO9-PsKhyao9TYfJ0k1mUrg8WN40~fCeH4tNAj70m~B0H0qQEi79-FJB20B8yazYq5g3Y23OhI7vT0uQDSo1L6bv-CAw__",
+                    }}
+                    style={styles.chatProfileImage}
+                  />
+                )}
+                <View
+                  style={[
+                    styles.bubble,
+                    message.origin === "Austin"
+                      ? styles.bubbleMine
+                      : styles.bubbleTheirs,
+                  ]}
+                >
+                  <Text style={styles.bubbleText}>{message.message}</Text>
+                </View>
+              </View>
+            ))}
+          </ScrollView>
+        </View>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            autoCapitalize="none"
+            value={newMessageText}
+            placeholder="Enter text here..."
+            onChangeText={(newMsg) => setNewMessageText(newMsg)}
+          />
+          <TouchableOpacity style={styles.btn} onPress={btnSendMsg}>
+            <Text style={styles.buttonText}>Send</Text>
+          </TouchableOpacity>
+        </View>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    marginTop: "80%",
-    width: "100%",
-    height: "10%",
+  safeArea: {
+    flex: 1,
+    backgroundColor: "white", // Or any other light shade you want for the background
+  },
+  profileImage: {
+    width: 60,
+    height: 60,
+    borderRadius: 10,
+    // alignSelf: "center",
+  },
+  chatProfileImage: {
+    width: 36,
+    height: 36,
+    borderRadius: 10,
+    // alignSelf: "center",
+  },
+  confirmButton: {
+    flexDirection: "row",
+    backgroundColor: "#28637D",
     alignItems: "center",
-    // borderWidth: 1,
-    justifyContent: "center",
+    borderRadius: 100,
+    // height: 45,
+    // paddingVertical: 5,
+    paddingHorizontal: 10,
+  },
+  confirmButton1: {
+    flexDirection: "row",
+    backgroundColor: "#EAEAEA",
+    alignItems: "center",
+    borderRadius: 100,
+    // height: 20,
+    // paddingVertical: 10,
+    paddingHorizontal: 10,
+  },
+  confirmButtonText: {
+    fontFamily: "Poppins-SemiBold",
+    color: "white",
+    fontSize: 18,
+    marginLeft: 10,
+  },
+  confirmButtonText1: {
+    fontFamily: "Poppins-Bold",
+    color: "#EAEAEA",
+    fontSize: 18,
+    marginLeft: 10,
+  },
+  topHeader: {
+    marginTop: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
+  },
+  userName: {
+    fontFamily: "Poppins-Bold",
+    fontSize: 20,
+    marginTop: 5,
+  },
+  date: {
+    fontFamily: "Poppins-Regular",
+    fontSize: 20,
+    marginTop: 5,
+  },
+  address: {
+    fontFamily: "Poppins-Regular",
+    fontSize: 20,
+    marginTop: 5,
+  },
+  header: {
+    // flexDirection: 'row',
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "100%",
+    paddingHorizontal: 16,
+    paddingVertical: 10,
+    backgroundColor: "#EAEAEA", // Or any other color you want for the header
+  },
+  container: {
+    // marginTop: "80%",
+    // width: "100%",
+    // height: "10%",
+    // alignItems: "center",
+    // // borderWidth: 1,
+    // justifyContent: "center",
+    flex: 1,
   },
 
   inputContainer: {
     width: "100%",
+    paddingHorizontal: 10,
     flexDirection: "row",
-  },
-
-  divider: {
-    borderColor: "black",
-    borderBottomWidth: 10,
-    marginVertical: 0,
-    width: "100%", // Adjust this value to control the space above and below the divider
+    backgroundColor: "transparent",
   },
 
   input: {
     width: "70%",
     height: 40,
     marginRight: 10,
-    borderWidth: 1,
-    borderColor: "gray",
-    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: "rgba(40, 99, 125, 0.3)",
+    borderRadius: 100,
+    paddingHorizontal: 10,
+    fontFamily: "Poppins-Regular",
+    fontSize: 18,
   },
 
   btn: {
-    backgroundColor: "blue",
-    borderRadius: "100%",
-    width: "25%",
+    backgroundColor: "#28637D",
+    borderRadius: 100,
+    paddingHorizontal: 20,
     height: 40,
     alignItems: "center",
     justifyContent: "center",
-    marginBottom: 200,
+  },
+
+  buttonText: {
+    fontFamily: "Poppins-Regular",
+    fontSize: 18,
+    color: "white",
   },
 
   msgArea: {
     width: "100%",
     height: 500,
-    backgroundColor: "grey",
+    backgroundColor: "white",
     flexGrow: 1,
+    padding: 10,
+  },
+
+  bubble: {
+    padding: 10,
+    borderRadius: 10,
+    marginBottom: 5,
+  },
+
+  bubbleMine: {
+    backgroundColor: "#EAEAEA",
+    borderRadius: 10,
+  },
+
+  bubbleTheirs: {
+    marginLeft: 5,
+    backgroundColor: "rgba(40, 99, 125, 0.3)",
+    borderRadius: 10,
+  },
+
+  messageContainer: {
+    flexDirection: "row",
+  },
+
+  bubbleText: {
+    fontFamily: "Poppins-Regular",
+    fontSize: 18,
   },
 
   msgAreaContainer: {
     width: "100%",
-    height: 400,
+    flex: 1,
+  },
+
+  messageTheirs: {
+    // backgroundColor: "#EAEAEA", // Example background color for own messages
+    alignSelf: "flex-start",
+    // borderRadius: 10,
   },
 
   messageMine: {
     alignSelf: "flex-end", // Align message containers to the right side
-    backgroundColor: "lightblue", // Example background color for own messages
+    // backgroundColor: "rgba(40, 99, 125, 0.3)", // Example background color for own messages
+    // borderRadius: 10,
   },
 });
 
